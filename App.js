0506a9a8e66b928b8fe3898f76a323e7ab677a9f
kawanneramos <161034609+kawanneramos@users.noreply.github.com>
2024-03-08 09:26:12 -0300

    08-03

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native'; 
 
-import Exemplo_0 from './componentes/exemplo';
+import Exemplo_0 from './componentes/exemplo_1';
 
 export default function App() {
   return (
