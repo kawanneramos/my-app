2c0083f9eef838e3525eb574d5d41dce6211537b
kawanneramos <161034609+kawanneramos@users.noreply.github.com>
2024-03-19 15:23:19 -0300

    aula19-03

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native'; 
 
 import Exemplo_0 from './componentes/exemplo_1';
-
+import Exemplo_2 from './componentes/exemplo_2';
 
 import Atividade_1 from './componentes/atividade_1';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Atividade_1 />
+      <Exemplo_2 />
       <StatusBar style="auto" />
     </View>
   );
---
+++ componentes/exemplo_2/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import styles from './styles';
+function Exemplo_2 () {
+    return(
+        <View style={styles.container}>
+            <Text>Exemplo2</Text>
+        </View>
+    );
+}
+export default Exemplo_2;
