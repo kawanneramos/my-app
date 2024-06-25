b144777a4480f7ed74f28c28f5137c47894f67ac
kawanneramos <161034609+kawanneramos@users.noreply.github.com>
2024-06-25 14:55:12 -0300

    25-06

--- componentes/exemplo_4/index.js
+++ componentes/exemplo_4/index.js
@@ -1,14 +1,17 @@
-import { View, Text, TextInput} from 'react';
+import { View, Text, TextInput } from 'react-native';
+
 import styles from './styles';
+
 export default function Exemplo_4 () {
     return(
         <View style={styles.container}>
             <Text style={styles.titulo}>Exemplo 4</Text>
-            <TextInput 
-            placeholder='digite seu nome...' 
+
+            <TextInput
+             placeholder='Digite seu nome...' 
             style={styles.input}
             />
-            <Text style={styles.texto}> Nome digitado: </Text>
+            <Text style={styles.texto}>Nome digitado: </Text>
         </View>
     );
 }
--- componentes/exemplo_4/styles.js
+++ componentes/exemplo_4/styles.js
@@ -2,18 +2,34 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#e67a96',
-      borderRadius:20,
-      width: '100%',
-      alignItems: 'center',
+        flex: 1,
+        backgroundColor: '#fc9ae9',
+        borderRadius: 20,
+        width: '100%',
+        alignItems: 'center',
     },
     titulo: {
         fontSize: 30,
         fontWeight: 'bold',
-        marginVertical:20,
-        color: '#673AB7',
+        marginVertical: 20,
+        color: '#eb34b4',
     },
-} );
+    input: {
+        fonytSize: 30,
+        color: '#eb34b4',
+        width: '80%',
+        marginVertical: 10,
+        borderWidth: 2,
+        borderColor: '#eb34b4',
+        padding: 5,
+        borderRadius: 20,
+    },
+    texto: {
+        fontSize: 25,
+        color: '#eb34b4',
+        width: '80%',
+        marginVertical: 10,
+    },
+});
 
 export default styles;
